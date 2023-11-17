c3988ff953cdb8e0a439eba2bb74126c8e6c89e7
Simon Socolow <sms12@williams.edu>
2023-11-16 21:58:28 -0500

    done

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -1,24 +1,25 @@
 module Evaluator
 
-type Date = string
+// type Date = string
+type Date = int
 
 type Time = float
 
-type h2o = Time
+type H2o = Time
 
-type Activity =
-| h2o list
+type Activity = H2o list
+
+type Day = {date: Date; activity: Activity}
+
+type History = Day list
 
 let CANVAS_SZ = 1000
 
-//keep track of how many refills
-let cumulator = 0
-
-type Coordinate = { x: int; y: int }
+type Coordinate = { x: float; y: float }
 
 let startingPoint = "122, 734 "
-let startX = 122
-let startY = 734
+let startX = 122.0
+let startY = 734.0
 let rightStep = 36
 let upStep = 76
 let start = {x=startX; y=startY}
@@ -29,9 +30,10 @@ let start = {x=startX; y=startY}
 //     | Blue -> "rgb(0,0,255)"
 //     | Purple -> "rgb(128,8,165)"
 
-let evalSingleActivity (h2o: h2o) (whichOne: int) : string =
-    let x = string (startX * 35.875 + 122)
-    let y = string (startY - whichOne * 76)
+let evalSingleActivity (h2o: H2o) (whichOne: int) : string =
+    let x = string (startX + (h2o / 100.0) * 35.875)
+    let y = string (startY - ((float whichOne) * 76.0))
+    // printfn $"{whichOne} for y = {y} x = {x} h2o = {h2o}"
     (x + ", " + y + " ")
 
 
@@ -39,17 +41,23 @@ let evalSingleActivity (h2o: h2o) (whichOne: int) : string =
 let rec evalActivities (activity: Activity) (whichOne: int): string =
     match activity, whichOne with
     | [],_ -> ""
-    | l::ls,x -> (evalSingleActivity l x) + (evalActivities ls (x-1))
+    | l::ls,x -> (evalActivities ls (x-1)) + (evalSingleActivity l x)
 
 let startEval (activity: Activity): string =
     let num = List.length activity
-    evalActivities activity num
+    let newact = List.rev activity
+    evalActivities newact num
 
-let eval (activity: Activity) : string =
+let eval (day: Day) : string =
     let csz = CANVAS_SZ |> string
+    let len = List.length ((fun a -> a.activity) day)
+    let points = startEval ((fun a -> a.activity) day)
+
+    let lastPoint = "983, " + string (startY - ((float len) * 76.0))
+
     "<svg width=\"" + csz + "\" height=\"" + csz + "\"" +
     " xmlns=\"http://www.w3.org/2000/svg\"" +
     " xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n" +
-    "<polyline points=\"" + startingPoint + (evalActivities Activity) + "\" stroke=\"blue\" fill=\"none\" style=\"stroke: blue; stroke-width: 10;\"/>" +
-    " <image href=\"health_data_graph.svg\" x=\"0\" y=\"0\" height=\"1500\" width=\"1500\"/>\n" +
+    "<polyline points=\"" + startingPoint + points + lastPoint + "\" stroke=\"blue\" fill=\"none\" style=\"stroke: blue; stroke-width: 5;\"/>\n" +
+    " <image href=\"https://raw.githubusercontent.com/ninjahat77/pastebin/main/health_data_graph.svg\" x=\"0\" y=\"0\" height=\"1500\" width=\"1500\"/>\n" +
     "</svg>\n"
--- code/Parser.fs
+++ code/Parser.fs
@@ -1,74 +1,29 @@
 module Parser
 
+open Evaluator
 open Combinator
 open System
 
 (*GRAMMAR*)
-let number = (pmany1 (pdigit |>> fun ds -> ds |> stringify |> int))
-let date = pright (pstring ("date ")) ((pleft number pchar ('-')) pseq(pleft(number pchar ('-')) number (fun (a,b) -> ) )) //convert this 
-let fillh20 = pright (pstring("h20 ")) (pmany1 (pdigit |>> fun ds -> ds |> stringify |> float)) //convert fill h20 time 
+let number = (pmany1 pdigit) |>> (fun ds -> stringify ds |> int)  <!> "number"
+//convert this
+// let date = pright (pstr ("date ")) ((pleft number (pchar '-')) pseq(pleft(number pchar ('-')) number (fun (a,b) -> ) )) <!> "date"
+let date = pright (pstr ("date ")) number <!> "date"
+
+//convert fill h2o time
+// let fillh2o = pright (pstr("h2o ")) (pmany1 (pdigit |>> (fun ds -> ds |> stringify |> float))) <!> "fillh20"
+let fillh2o = (pright (pstr("h2o ")) ((pmany1 pdigit) |>> (fun ds -> ds |> stringify |> float))) <|> pright (pstr(" h2o ")) ((pmany1 pdigit) |>> (fun ds -> ds |> stringify |> float)) <!> "fillh2o"
 
 //make into their respective types 
-let exp = date <|> pmany0 fillh20
+let expr = pseq date (pmany0 fillh2o) (fun (a, b) -> {date = a; activity = b}) <!> "exp"
 
-
-
-
-
-
-
-
-let r = new Random()
-
-(*
- *   <expr> ::= <line><expr>
- *           |  repeat<n><line><expr>
- *           |  <empty>
- *   <line> ::= <color> line
- *      <n> ::= (is any positive integer)
- *  <color> ::= red | green | blue | purple
- *)
-
-let randomLine (color: Color) : Line =
-    let a = { x = r.Next CANVAS_SZ; y = r.Next CANVAS_SZ}
-    let b = { x = r.Next CANVAS_SZ; y = r.Next CANVAS_SZ}
-    { c1 = a; c2 = b; color = color }
-
-let pad p = pbetween pws0 p pws0
-
-let expr, exprImpl = recparser()
-
-let color =
-    (pstr "red" |>> (fun _ -> Red)) <|>
-    (pstr "green" |>> (fun _ -> Green)) <|>
-    (pstr "blue" |>> (fun _ -> Blue)) <|>
-    (pstr "purple" |>> (fun _ -> Purple))
-
-let line =
-    pleft
-        (pad color)
-        (pad (pstr "line"))
-    |>> randomLine
-
-let n = pmany1 pdigit |>> (fun ds -> stringify ds |> int)
-
-let repeat =
-    pseq
-        (pright (pad (pstr "repeat")) (pad n))
-        line
-        (fun (i, l) ->
-            [0..i-1] |> List.map (fun _ -> randomLine l.color)
-        )
-exprImpl :=
-    pmany1 (
-        line |>> (fun l -> [l]) <|>
-        repeat
-    ) |>> List.concat
 
 let grammar = pleft expr peof
 
-let parse (input: string) : Canvas option =
-    let i = prepare input
+let parse (input: string) : Day option =
+    let DEBUG = false
+    //let i = prepare input
+    let i = if DEBUG then debug input else prepare input
     match grammar i with
     | Success(ast, _) -> Some ast
     | Failure(_,_) -> None
